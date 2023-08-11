aabb453852e665a6295992045121c078b7a127d3
Kevin-kinyanjui <kevin.kinyanjui@student.moringaschool.com>
2023-08-11 13:47:02 +0300

    removed timestamps

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -2,7 +2,7 @@ class RestaurantsController < ApplicationController
     rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
     def index
-        render json: Restaurant.all
+        render json: Restaurant.all, except: [:created_at, :updated_at]
     end
 
     def show
