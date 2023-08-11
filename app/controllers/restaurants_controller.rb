8fed369bb31a308cd82abf28095bc7a466e546c1
Kevin-kinyanjui <kevin.kinyanjui@student.moringaschool.com>
2023-08-11 13:40:14 +0300

    finished resturant controller

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,2 +1,28 @@
 class RestaurantsController < ApplicationController
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+
+    def index
+        render json: Restaurant.all
+    end
+
+    def show
+        rest = find_resturant
+        render json: rest
+    end
+
+    def destroy
+        rest = find_resturant
+        rest.destroy
+        head :no_content
+    end
+
+    private
+
+    def find_resturant
+        Restaurant.find(params[:id])
+    end
+
+    def not_found_response
+        render json: { error: "Restaurant not found" }, status: :not_found
+      end
 end
